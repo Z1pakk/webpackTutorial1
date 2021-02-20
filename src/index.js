f38f808c90b62b8adb54bd2df3256d6769c2d37f
Z1pakk <vlad.snow@bbconsult.co.uk>
2021-02-20 16:48:35 +0200

    Add json file and set configuration to watch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Post from './Post'
 import './styles/styles.css'
+import json from './assets/json.json'
 
 const post = new Post('Webpack Post Title')
 
 console.log(post.toString())
+
+console.log(json)
