36cde895f76d6675dad071feb0379582d0ba5b0d
Z1pakk <vlad.snow@bbconsult.co.uk>
2021-02-20 17:12:08 +0200

    Add xml-loader

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import Post from './Post'
 import './styles/styles.css'
 import json from './assets/json.json'
 import WebpackLogo from './assets/webpack-logo.png'
+import xml from './assets/data.xml'
 
 const post = new Post('Webpack Post Title', WebpackLogo)
 
 console.log(post.toString())
 
 console.log(json)
+
+console.log('XML: ', xml)
