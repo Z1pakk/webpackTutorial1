97197db1d2536624d226a4f7a726ad6ac3dce2dc
Z1pakk <vlad.snow@bbconsult.co.uk>
2021-02-20 20:14:32 +0200

    Configure typescript for babel

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
     mode: 'development',
     entry: {
         main: ["@babel/polyfill", "./index.js"],
-        analytics: './analytics.js'
+        analytics: './analytics.ts'
     },
     output: {
         filename: filename('js'),
@@ -127,7 +127,25 @@ module.exports = {
                 use: {
                     loader: "babel-loader",
                     options: {
-                        presets: ['@babel/preset-env'],
+                        presets: [
+                            '@babel/preset-env',
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.m?ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-typescript'
+                        ],
                         plugins: [
                             '@babel/plugin-proposal-class-properties'
                         ]
