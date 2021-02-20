06e73444dad79e4ce4a97a9eda5ceeb415c3a097
Z1pakk <vlad.snow@bbconsult.co.uk>
2021-02-20 16:24:29 +0200

    Add CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -13,7 +14,7 @@ module.exports = {
     },
     plugins: [
         new HTMLWebpackPlugin({
-            title: 'Webpack Vladilen'
+            template: './src/index.html'
         })
     ]
 }
