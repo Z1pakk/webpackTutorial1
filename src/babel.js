017c3d8e191bf68a9630d1e0466c9453062cb4b5
Z1pakk <vlad.snow@bbconsult.co.uk>
2021-02-21 19:48:08 +0200

    Lint project and add additional configuration for prod

--- src/babel.js
+++ src/babel.js
@@ -1,19 +1,21 @@
-async function start() {
-  return Promise.resolve('async is working');
-}
-
-start().then((r) => {
-  // console.log(r);
-});
-
-// const unused = 42;
-//
-// class Util {
-//     static id = Date.now()
+// async function start() {
+//   return Promise.resolve('async is working');
 // }
 //
-// console.log(Util.id);
-
-import('lodash').then((_) => {
-  // console.log('Lodash', _.random(0, 42, true));
-});
+// start().then((r) => {
+//   console.log(r);
+// });
+//
+// console.log('test')
+//
+// // const unused = 42;
+// //
+// // class Util {
+// //     static id = Date.now()
+// // }
+// //
+// // console.log(Util.id);
+//
+// import('lodash').then((_) => {
+//   console.log('Lodash', _.random(0, 42, true));
+// });
